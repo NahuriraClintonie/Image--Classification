2c69e471db0b2d72c459b2b1d30dbfe956679cb8
NahuriraClintonie <clintonnahurira24@gmail.com>
2024-11-08 08:55:45 +0000

    Add data_utils.py with CIFAR-10 data loading function

---
+++ src/data_utils.py
@@ -0,0 +1,6 @@
+
+import tensorflow as tf
+
+def load_cifar10_data():
+    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
+    return (x_train, y_train), (x_test, y_test)
---
+++ src/explore_data.py
@@ -0,0 +1,16 @@
+
+import matplotlib.pyplot as plt
+from data_utils import load_cifar10_data
+
+def show_sample_images(x, y, num_samples=5):
+    plt.figure(figsize=(10, 2))
+    for i in range(num_samples):
+        plt.subplot(1, num_samples, i + 1)
+        plt.imshow(x[i])
+        plt.title(f"Label: {y[i][0]}")
+        plt.axis('off')
+    plt.show()
+
+# Load CIFAR-10 data and display some samples
+(x_train, y_train), _ = load_cifar10_data()
+show_sample_images(x_train, y_train)
